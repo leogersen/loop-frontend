f75e069d0e2502a51b825973d79771025dbc603d
leogersen <46613350+leogersen@users.noreply.github.com>
2021-01-29 11:37:35 -0300

    Coding NavBar

---
+++ src/components/NavBar.js
@@ -0,0 +1,52 @@
+import React, { Component } from 'react';
+import { APP_NAME } from '../constants';
+import NavBarItem from './NavBarItem';
+
+
+class NavBar extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+
+            items: [
+                { name: "Listar Tarefas", href: "/" },
+                { name: "Nova Tarefa", href: "/form" },
+            ]
+        }
+
+    }
+    onClickHandler(item) {
+        alert(item.name);
+
+    }
+
+    render() {
+        return (
+            <div>
+
+                <nav className="navbar navbar-expand-lg navbar-light bg-light">
+                    <span className="navbar-brand mb-0 h1">{APP_NAME}</span>
+                    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarText">
+                        <div className="navbar-nav mr-auto">
+
+                            {this.state.items.map(
+                                i => <NavBarItem
+                                    item={i}
+                                    onClick={this.onClickHandler} />)}
+
+                        </div>
+
+                    </div>
+                </nav>
+               
+            </div>
+        );
+    }
+}
+
+export default NavBar;
