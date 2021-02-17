a302f4a7c431efed7f48e44f4bfccd9149b33a8d
Leo Gersen <leogersen@outlook.com>
2021-02-17 02:49:16 -0300

    Constants

--- src/constants.js
+++ src/constants.js
@@ -1,1 +1,6 @@
+const SERVER = process.env.REACT_APP_SERVER;
+
 export const APP_NAME = "LoopTasks";
+export const AUTH_SERVICE = `${SERVER}`
+export const APP_ENDPOINT = `${SERVER}/api`
+export const JWT_TOKEN_NAME = "jwt";
